dc73fc5fda7fdf33f3615f7cb84542a2436f1e3a
Hyper Jones <47111201+hyperjones@users.noreply.github.com>
2022-05-18 15:32:14 +0800

    Update repeatedDigits.c

--- repeatedDigits.c
+++ repeatedDigits.c
@@ -4,32 +4,36 @@
 int main()
 {
    bool digit_seen[10] = { false };
+   bool repeated_digit[10] = { false };
    printf("Enter a number: ");
    long n;
    scanf("%ld", &n);
    
    int digit = 0;
-   printf("Repeated digit(s): ");
    while( n > 0 )
    {
-      //gets the last digit of input number
-      digit = n % 10; 
+      digit = n % 10;
 
-     //prints out the seen digit
+      //if the digit has seen twice
+      //the digit is repeated.
       if(digit_seen[digit])
-         printf("%ld ", digit);
-
-      //assign true if the digit hasn't seen yet
+         repeated_digit[digit] = true;
+       
+      //assign true if the digit hasn't seen 
       if(!digit_seen[digit])
          digit_seen[digit] = true;
        
-       n /= 10; //removes the last digit
-       digit++; //What the fuck is this?
+       n /= 10;
+       digit++;
    }
-   
+  
+   printf("Repeated digit(s): ");
+   for(int i = 0; i < 10; i++)
+   {
+      //prints out the repeated digit
+      if(repeated_digit[i])
+         printf("%d ", i);
+   }
+         
    return 0;
 }
-/*
- this program produces output of repeated digit more than 1
- repeated digit should be printed only one time
-*/
